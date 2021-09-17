5315f742853652421808734c9d0d53dc9ea7098c
Sk8056 <sarangkumar8056@gmail.com>
2021-09-18 01:34:08 +0530

    more questions on binary search

--- programming/BinarySearch.java
+++ programming/BinarySearch.java
@@ -25,7 +25,7 @@ public class BinarySearch {
     }
 
     /**
-     *  ceiling of a number using binary search
+     *  ceiling of a number in sorted array using binary search.
      *  find number greater than or equal to the given number.
      */
      static int ceiling(int[] ar, int target) {
@@ -45,11 +45,11 @@ public class BinarySearch {
                 left = mid + 1;
             } 
         }
-        return left > 0 ? left : -1;
-     }
+        return left;
+    }
 
      /**
-     *  floor of a number using binary search
+     *  floor of a number in sorted array using binary search
      *  find number less than or equal to the given number.
      */
      static int floor(int[] ar, int target) {
@@ -69,9 +69,12 @@ public class BinarySearch {
                 left = mid + 1;
             } 
         }
-        return right > 0 ? right : -1;
-      }
+        return right;
+    }
       
+      /**
+       *  leetcode Q.744
+       */
       static char nextGreatestLetter(char[] letters, char target) {
           int length = letters.length;
           int left = 0;
@@ -92,15 +95,56 @@ public class BinarySearch {
                 }
           }
           return letters[left];
-      }
+    }
+
+      /**
+       * The change is no need to find the mid, we can use modulo concept to find the answer.
+       * Basically we have to violate the while loop condition which will bring left position to
+       * correct position. Modulo example 4 % 4 = 0.
+       */
+      static char nextGreatestLetterImproved(char[] letters, char target) {
+          int left = 0;
+          int right = lettters.length - 1;
+          while(right >= left) {
+            int mid = left + (right - left) / 2;
+            if(target < letters[mid]) right = mid - 1;
+            else left = mid + 1;
+          }
+          return letters[left % letters.length];
+    }
+    
+      /**
+       * leetcode Q.34
+       */
+      static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};  
+        if(nums.length == 0) return ans;
+        int left = searchPosition(nums, target, true);
+        int right = searchPosition(nums, target, false);
+        ans[0] = left; ans[1] = right;
+        return ans;
+    }
+    
+      static int searchPosition(int[] nums, int target, boolean findFirstIndex) {
+        int ans = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while(right >= left) {
+           int mid = left + (right - left) / 2;
+           if(nums[mid] > target) right = mid - 1;
+           else if(nums[mid] < target) left = mid + 1;
+           else {
+               ans = mid;
+               if(findFirstIndex) right = mid - 1;
+               else left = mid + 1;
+           }
+        }  
+        return ans;        
+    }
 
       
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] array = {10,12,23,45,60};
-        int input = scanner.nextInt();
-        int result = floor(array, input);
-        System.out.println("index " + result + " number " + array[result]);
+        
     }
         
 }
